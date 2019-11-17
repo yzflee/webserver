23a4a5826503745962a974a59a04a0ed373e229a
joungtao <joungtao@gmail.com>
2019-11-17 12:15:38 +0800

    update server/app.py
    # Conflicts:
    #	server/app.py

--- server/app.py
+++ server/app.py
@@ -279,7 +279,7 @@ class application(object):
 			object.__setattr__(self, 'content', self.content + str(content))
 
 		def __setattr__(self, key, value):
-			self.content += '<script type="text/javascript">var %s = %s;</script>' % (key, json.dumps(value) or "null")
+			object.__setattr__(self, 'content', self.content + '<script type="text/javascript">var %s = %s;</script>' % (key, json.dumps(value) or "null"))
 		def __setattr__(self, key, value):
 			self.content += '<script type="text/javascript">var %s = %s;</script>' % (key, json.dumps(value) or "null")
 		def __setattr__(self, key, value):
