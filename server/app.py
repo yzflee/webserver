e1ffb2cd911d8a643b6abd69e8e3ff42077c1461
joungtao <joungtao@gmail.com>
2019-11-17 11:40:04 +0800

    update server/app.py
    # Conflicts:
    #	server/app.py

--- server/app.py
+++ server/app.py
@@ -273,13 +273,13 @@ class application(object):
 
 	class TemplateOutput(object):
 		def __init__(self):
-			object.__setattr__(self, 'content', '')
+			self.content = ''
 
 		def __getitem__(self, content):
-			object.__setattr__(self, 'content', self.content + str(content))
+			self.content += str(content)
 
 		def __setattr__(self, key, value):
-			object.__setattr__(self, 'content', self.content + '<script type="text/javascript">var %s = %s;</script>' % (key, json.dumps(value) or "null"))
+			self.content += '<script type="text/javascript">var %s = %s;</script>' % (key, json.dumps(value) or "null")
 
 	def template(self, path, data):
 		path = os.path.join(config.TEMPLATE, path.lstrip('/'))
