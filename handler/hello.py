e365073d49229c00521d90368cdba69f5609c429
wzj <2540379881@qq.com>
2019-11-19 18:43:02 +0800

    add branches.py

--- handler/hello.py
+++ handler/hello.py
@@ -5,9 +5,17 @@ import logic.Team1902.projectInfo as P
 import logic.Team1902.branches as Q
 import logic.Team1902.commits as C
 @route('/hello.py.html')
-def projectInfo(cookies):
+def projectInfo(cookie):
 
 	info = [P.getProjectInfo(),Q.getbranches(),C.getcommits()]
+	infoShow  = [
+  	{ 'genre': 'Sports', 'sold': 275 },
+  	{ 'genre': 'Strategy', 'sold': 115 },
+  	{ 'genre': 'Action', 'sold': 120 },
+  	{ 'genre': 'Shooter', 'sold': 350 },
+  	{ 'genre': 'Other', 'sold': 150 }
+	]
+
 
 	#将info返回给页面
-	return response(projectInfo=info)
+	return response(projectInfo=info,infoShow = infoShow)
